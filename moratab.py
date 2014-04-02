76eddda7fedc303453f84ca9780dab9edb2cbf39
Alireza Nourian <alireza.nournia@gmail.com>
2014-04-02 19:13:39 +0430

    add server moratab parser

---
+++ moratab.py
@@ -0,0 +1,24 @@
+
+import re, mistune
+
+ltr = re.compile(r'[ <>*+\t\n\\\/\[\($-]*[A-Za-z]')
+refine = lambda html: refine(html[html.find('>')+1:]) if html.startswith('<') else html
+direction = lambda html: ' dir="ltr"' if ltr.match(refine(html)) else ''
+
+
+class Moratab(mistune.Renderer):
+	def paragraph(self, text):
+		return '<p%s>%s</p>\n' % (direction(text), text)
+
+	def list_item(self, text):
+		return '<li%s>%s</li>\n' % (direction(text), text)
+
+	def block_quote(self, text):
+		return '<blockquote%s>%s\n</blockquote>' % (direction(text), text)
+
+	def table_cell(self, content, **flags):
+		tag = 'th' if flags['header'] else 'td'
+		align = flags['align']
+		if not align:
+			return '<%s>%s</%s>\n' % (tag, content, tag)
+		return '<%s align="%s">%s</%s>\n' % (tag, align, content, tag)
