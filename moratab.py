17f6615ec440cee2e513a0d5fd9531a2813d29d2
Alireza Nourian <alireza.nournia@gmail.com>
2014-05-14 15:37:54 +0430

    replace math expressions before markdown rendering

--- moratab.py
+++ moratab.py
@@ -27,8 +27,30 @@ class Moratab(mistune.Renderer):
 		return '<%s align="%s">%s</%s>\n' % (tag, align, content, tag)
 
 
-markdown = mistune.Markdown(renderer=Moratab())
+markdown = mistune.Markdown(renderer=Moratab(), hard_wrap=True)
+
+
+def replace_expressions(text):
+	expressions = {}
+
+	def expkey(match):
+		key = 'x1x1x%dx' % (len(expressions)-1)
+		expressions[key] = match.group(1)
+		return key
+
+	return re.sub(r'(\$\$?[^\$\n]+\$?\$)', expkey, text), expressions
 
 
 def render(text):
-	return markdown.render(text)
+
+	# remove expressions
+	text, expressions = replace_expressions(text)
+
+	# render text without expressions
+	rendered = markdown.render(text)
+
+	# add expressions
+	for key, value in expressions.items():
+		rendered = rendered.replace(key, value)
+
+	return rendered
